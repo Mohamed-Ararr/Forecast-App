184fdb85246bb26e4ab287fd27e268b34b4ea689
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-07-08 15:44:09 +0000

    build custom app bar for city view

--- lib/ConstantValues.dart
+++ lib/ConstantValues.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 const EdgeInsets kPaddingAll10 = EdgeInsets.all(10);
 const EdgeInsets kPaddingLR15 = EdgeInsets.symmetric(horizontal: 15);
 BorderRadius kBorderRad15 = BorderRadius.circular(15);
+BorderRadius kBorderRad10 = BorderRadius.circular(10);
 List<String> kStatList = [
   "Max temp",
   "Min temp",
--- lib/Features/CityView/Presentation/Widgets/CityViewBody.dart
+++ lib/Features/CityView/Presentation/Widgets/CityViewBody.dart
@@ -1,10 +1,30 @@
 import "package:flutter/material.dart";
+import "package:go_router/go_router.dart";
+import "package:weather_app/ConstantValues.dart";
+import "package:weather_app/Core/AppColors.dart";
+import "package:weather_app/Features/CityView/Presentation/Widgets/CustomAppBar.dart";
+
+import "../../../../Core/AppFonts.dart";
 
 class CityViewBody extends StatelessWidget {
   const CityViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: kPaddingLR15,
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const CustomAppBar(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
