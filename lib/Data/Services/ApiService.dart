ba07fbe938eaba4020321b2f377a8044506acbfb
Mohamed Islam Arrar <mohamedarrar953@gmail.com>
2023-07-09 13:30:14 +0000

    build apiService

---
+++ lib/Data/Services/ApiService.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final String _apiKey = "3ca15d1ff34c44b2983141929230207";
+  final String _baseUrl = "http://api.weatherapi.com/v1/forecast.json?";
+  final Dio dio;
+
+  ApiService(this.dio);
+
+  Future<Map<String, dynamic>> get({required String endPoint}) async {
+    var response = await dio
+        .get("${_baseUrl}key=$_apiKey&q=$endPoint&days=10&aqi=no&alerts=no");
+    return response.data;
+  }
+}
